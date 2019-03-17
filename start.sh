3956ba5d252655fa71c90ea8379603788db3726d
Emmanuel law <emmanuel.law@gmail.com>
2019-03-16 23:01:57 -0700

    Fix Mac OSX compatibility issue with agent-token and password generation
    On OXS, tr by default expects input  as UTF8. By setting LC_CTYPE=c, it treats inputs as raw bytes.

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ if [ ! -f ./private_key.pem ]; then
   vapid --gen
 fi
 if [ ! -f ./.password ]; then
-  cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > .password
+  (export LC_CTYPE=C ; cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > .password)
 fi
 if [ ! -f ./.agenttoken ]; then
-  cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > .agenttoken
+  (export LC_CTYPE=C ; cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > .agenttoken)
 fi
 flask db upgrade
 mitmweb -k -s sw_mitm.py -p 8888 &
