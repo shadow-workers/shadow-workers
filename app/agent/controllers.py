41b5a49cda4b344eefcc01bc7830b19b0d8498d4
Emmanuel law <emmanuel.law@gmail.com>
2019-03-25 05:06:33 +0800

    Consistency naming of using "user_agent"

--- app/agent/controllers.py
+++ app/agent/controllers.py
@@ -47,7 +47,7 @@ def geturl():
     ConnectedAgents[agentID]['ip'] = safeParam(request.remote_addr)
     ConnectedAgents[agentID]['last_seen'] = time.time()
     ConnectedAgents[agentID]['active'] = 'true'
-    ConnectedAgents[agentID]['user-agent'] = safeParam(request.headers.get('User-Agent'))
+    ConnectedAgents[agentID]['user_agent'] = safeParam(request.headers.get('User-Agent'))
     
     updateAgent(agentID, ConnectedAgents[agentID])
     
@@ -147,7 +147,7 @@ def updateAgent(agentID, params):
     now = datetime.now()
     agent = db.session().query(Agent).filter(Agent.id == agentID).first()
     if agent is None:
-        agent = Agent(agentID, now, now, params['domain'], params['port'], params['ip'], params['user-agent'])
+        agent = Agent(agentID, now, now, params['domain'], params['port'], params['ip'], params['user_agent'])
         db.session.add(agent)
         addModulesToNewAgent(agent)
         db.session.commit()
